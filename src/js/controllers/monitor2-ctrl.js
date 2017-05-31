958d7991ef81be826ecc398ce6a61a529c8b2eb4
Facundo Crego <facrego@ar.ibm.com>
2017-05-31 11:45:22 -0300

    add refresh button in monitor

--- src/js/controllers/monitor2-ctrl.js
+++ src/js/controllers/monitor2-ctrl.js
@@ -11,16 +11,24 @@ function Monitor2Ctrl($scope, $state, ApiService) {
     $scope.dataready = false;
     $scope.headerOffset = 210;
 
-    var sessions = JSON.parse(localStorage.getItem('sessions'));
-    if (sessions) {
-        for (var key in sessions) {
-            var item = sessions[key];
-            $scope.sessions.push({
-                id: item.id,
-                name: item.user.firstName + ' ' + item.user.lastName,
-                description: 'Timestamp: ' + item.checkin.shared.timestamp 
-            });
-            $scope.sessionsById[item.id] = item;
+    var load = function() {
+        var sessions = JSON.parse(localStorage.getItem('sessions'));
+        $scope.session = JSON.parse(localStorage.getItem('sessionSelected'));
+        if (sessions) {
+            for (var key in sessions) {
+                var item = sessions[key];
+                var selectableItem = {
+                    id: item.id,
+                    name: item.user.firstName + ' ' + item.user.lastName,
+                    description: 'Timestamp: ' + item.checkin.shared.timestamp 
+                };
+                $scope.sessions.push(selectableItem);
+                
+                if ($scope.session && item.id == $scope.session.id) {
+                    $scope.selectedSession = selectableItem;
+                }
+                $scope.sessionsById[item.id] = item;
+            }
         }
     }
 
@@ -34,6 +42,7 @@ function Monitor2Ctrl($scope, $state, ApiService) {
     $scope.$watch('selectedSession', function(ov, nv) {
         if (ov != nv && $scope.selectedSession) {
             $scope.session = $scope.sessionsById[$scope.selectedSession.id];
+            localStorage.setItem('sessionSelected', JSON.stringify($scope.session));
             $scope.dataready = true;
         }
     });
@@ -52,11 +61,19 @@ function Monitor2Ctrl($scope, $state, ApiService) {
             $scope.sessions.splice(index, 1);
             delete sessions[id];
             $scope.selectedSession = null;
+            localStorage.setItem('sessionSelected', null);
             $scope.session = null;
             $scope.dataready = false;
             localStorage.setItem('sessions', JSON.stringify(sessions));
         }
-    }
+    };
+
+    $scope.refresh = function() {
+        $scope.sessions.length = 0;
+        load();
+    };
+
+    load();
 
     $scope.selectedOption = 'checkin';
 }
